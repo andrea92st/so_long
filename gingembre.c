93f440605138ac8cd475d58b3cd0246152b65c91
Andrea Fiorito <anfiorit@f2r8s5.clusters.42paris.fr>
2025-06-17 14:30:54 +0200

    weeee

--- gingembre.c
+++ gingembre.c
@@ -6,13 +6,13 @@
 /*   By: anfiorit <anfiorit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 13:22:03 by anfiorit          #+#    #+#             */
-/*   Updated: 2025/06/05 14:58:08 by anfiorit         ###   ########.fr       */
+/*   Updated: 2025/06/16 17:31:17 by anfiorit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int gamberge()
+int gamberge(char *str)
 {
     int fd;
     int gamberge_bien;
@@ -20,15 +20,15 @@ int gamberge()
     gamberge_bien = 0;
     fd = open(argv[1], O_RDONLY);
 
-    if (int check_wall(fd) == 1)
+    if (check_wall(fd) == 1)
         gamberge_bien++;
-    if (int check_exit(fd) == 1)
+    if (check_exit(fd) == 1)
         gamberge_bien++;
-    if (int check_player(fd) == 1)
+    if (check_player(fd) == 1)
         gamberge_bien++;
-    if (int check_item(fd) == 1)
+    if (check_item(fd) == 1)
         gamberge_bien++;
-    if (int check_ground(fd) == 1)
+    if (check_ground(fd) == 1)
         gamberge_bien++;
     if (gamberge_bien > 0)
         return (1);
